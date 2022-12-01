3a8717fd45c7bb798bde904b2a600afe45c1b3a0
Demmythetechie <demmythetechie@gmail.com>
2022-12-01 04:58:28 +0000

    5. 00...99

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python3
 for digit in range(0, 100):
-    if digit < 10:
-        print("{}".format(0))
-print("{}, "
+    if digit == 99:
+        print("99")
+    else:
+        print("{0:0=2d}, ".format(i), end='')
+
+        
