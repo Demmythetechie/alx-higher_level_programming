9fb53e8013469853f142da4173b3fbf8086316b7
Demmythetechie <demmythetechie@gmail.com>
2022-12-01 04:00:30 +0000

    12. Fizz Buzz

--- 0x01-python-if_else_loops_functions/12-fizzbuzz.py
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -5,8 +5,6 @@ def fizzbuzz():
             print("Fizz", end='')
         if mul % 5 == 0:
             print("Buzz", end='')
-        if mul % 3 != 0 or mul % 5 != 0:
-            print(mul, end='')
         if mul % 3 != 0 and mul % 5 != 0:
             print(mul, end='')
         if (mul == 99):
