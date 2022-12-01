124b56569fb88cea39a9d5c518dbfd67a13839e0
Demmythetechie <demmythetechie@gmail.com>
2022-12-01 03:20:23 +0000

    9. There are only 3 colors

--- 0x01-python-if_else_loops_functions/9-print_last_digit.py
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -5,9 +5,9 @@ def print_last_digit(number):
         last_f = str(number)
         last_s = '-' + last_f[-1]
         last = int(last_s)
-        return last
     elif number > 9:
         last = number % 10
-        return last
     else:
         return number
+return last
+print(last)
