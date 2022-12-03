e5f2708a8f80f94e25bfcfc500c9922ebdf3b376
Demmythetechie <demmythetechie@gmail.com>
2022-12-03 14:59:33 +0000

    2. My first toolbox!

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,10 +1,14 @@
 #!/usr/bin/python3
 if __name__  == '__main__':
     from sys import argv
-    print("{} argument".format(len(argv) - 1), end='')
     if len(argv) == 1:
+        print("{} arguments".format(len(argv) - 1), end='')
         print(".")
+    elif len(argv) == 2:
+        print("{} argument".format(len(argv) - 1), end='')
+        print(":")
     else:
+        print("{} arguments".format(len(argv) - 1), end='')
         print(":")
 
     for i in range(1, len(argv)):
