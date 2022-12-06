b3a59af01f2d7797f66d7b03fcb780d26f9ab4ca
Demmythetechie <demmythetechie@gmail.com>
2022-12-06 00:12:40 +0000

    4. Replace in a copy

---
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def new_in_list(my_list, idx, element):
+    ls = my_list
+    if idx < 0:
+        return my_list
+    elif idx >= 0:
+        return my_list
+    else:
+        ls[idx] = elements
+        return ls
