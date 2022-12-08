3ee4baf9f6e963939171aa2fb3fa0c885658f86f
Demmythetechie <demmythetechie@gmail.com>
2022-12-08 19:50:27 +0000

    13. Weighted average! and 14. Squared by using map

--- 0x04-python-more_data_structures/100-weight_average.py
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -1,9 +1,11 @@
 #!/usr/bin/python3
 def weight_average(my_list=[]):
     som = 0
+    num = 0
     for i in range(len(my_list)):
         tup = my_list[i]
+        num = num + tup[1]
         mul = tup[0] * tup [1]
         som = som + mul
-    average = som / (2 + 1 + 10 + 2)
+    average = som / num
     return average
