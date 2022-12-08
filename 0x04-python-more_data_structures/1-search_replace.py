8c3a19e9c9c738b3d09c2dce6d28ecb3f220f361
Demmythetechie <demmythetechie@gmail.com>
2022-12-08 08:48:11 +0000

    1. Search and replace

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -3,7 +3,7 @@ def search_replace(my_list, search, replace):
     ls = [my_list[x] for x in range(len(my_list))]
     for i in range(len(ls)):
         if ls[i] == search:
-            ls[i] = 89
+            ls[i] = replace
         else:
             pass
     return ls
