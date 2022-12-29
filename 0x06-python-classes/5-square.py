3ae9d254a3a391d6d25b39c0d2d09986b4d4fe26
demmythetechie@gmail.com <demmythetechie@gmail.com>
2022-12-29 11:09:49 +0000

    5. Printing a square

---
+++ 0x06-python-classes/5-square.py
@@ -0,0 +1,69 @@
+#!/usr/bin/python3
+""" This imports Square class """
+
+
+class Square:
+    """ This class caculate the square """
+
+    def __init__(self, size=0):
+        """ size variable was intialized
+
+        Args:
+            size (int): This carry out operation of a square
+
+        """
+        try:
+            if type(size) != int:
+                raise TypeError("size must be an integer")
+            elif size < 0:
+                raise ValueError("size must be >= 0")
+            else:
+                self.__size = size
+        except TypeError:
+            raise
+        except ValueError:
+            raise
+
+    @property
+    def size(self):
+        """ This is a getter for size attribute """
+
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """ This is the setter for size attribute
+
+        Args:
+            value (int): This helps with the modificationin setter
+
+        """
+
+        try:
+            if type(value) != int:
+                raise TypeError("size must be an integer")
+            elif value < 0:
+                raise ValueError("size must be >= 0")
+            else:
+                self.__size = value
+        except TypeError:
+            raise
+        except ValueError:
+            raise
+
+    def area(self):
+        """This method returns the square of size"""
+
+        s = self.__size ** 2
+        return s
+
+    def my_print(self):
+        """ This method prints the square of size using # """
+
+        if self.__size != 0:
+            for i in range(self.__size):
+                for i in range(self.__size):
+                    print("#", end='')
+                print()
+        else:
+            print()
--- 0x06-python-classes/README.md
+++ 0x06-python-classes/README.md
@@ -3,3 +3,4 @@ task 1: Created an empty class that defines a square with an private instance at
 task 2: Created an empty class that defines a square with an private instance attribute and checked if size was an integer
 task 3: From continuation of task 2 - i creadted a method are thats finds the square of size and returns it
 task 4: From continuation of task 3 - created a getter and setter for the private attribute size
+task 5: From continuation of task 4 - The square of size is used to print a shape squares using #
