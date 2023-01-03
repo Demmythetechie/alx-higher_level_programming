5d2d8a26ec1abe8c10a2d629d67c9122d923241b
demmythetechie@gmail.com <demmythetechie@gmail.com>
2023-01-03 02:05:49 +0000

    1. Real definition of a rectangle

---
+++ 0x08-python-more_classes/1-rectangle.py
@@ -0,0 +1,76 @@
+#!/usr/bin/python3
+"""
+    Create a class 'Rectangle' with two private attribute
+    'width and height' to build a rectangle
+"""
+
+
+class Rectangle:
+    """
+        The rectangle class creates two private attribute
+        along with a getter and setter for them both
+    """
+
+    def __init__(self, width=0, height=0):
+        """
+            Two private attributes are being intialised
+
+            Args:
+                Width (int): width of a rectangle
+                Height (int): height of a rectangle
+
+        """
+        self.__width = width
+        self.__height = height
+
+    @property
+    def width(self):
+        """ This is the getter for width """
+
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        """
+            This is the setter for width
+
+            Args:
+                value (int): width will contain value
+
+        """
+
+        try:
+            if type(value) != int:
+                raise TypeError("width must be an integer")
+            elif value < 0:
+                raise ValueError("width must be >= 0")
+            else:
+                self.__width = value
+        except (TypeError, ValueError):
+            raise
+
+    @property
+    def height(self):
+        """ This is the getter for height attribute """
+
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        """
+            This is the setter for height
+
+            Args:
+                value (int): height will contain value
+
+        """
+
+        try:
+            if type(value) != int:
+                raise TypeError("height must be an integer")
+            elif value < 0:
+                raise ValueError("height must be >= 0")
+            else:
+                self.__height = value
+        except (TypeError, ValueError):
+            raise
--- 0x08-python-more_classes/README.md
+++ 0x08-python-more_classes/README.md
@@ -1,1 +1,2 @@
 Task 1: Created an empty class Rectangle
+Task 2: Updated the class Rectangle by intializing two private attribute 'width and height' and created getter and setters for them
