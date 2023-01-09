8df01aff2ab3b2469e97bd28dfd611ed10999ba1
demmythetechie@gmail.com <demmythetechie@gmail.com>
2023-01-09 22:48:48 +0000

    1. My list

---
+++ 0x0A-python-inheritance/1-my_list.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""
+    This class inherits the superclass list
+    which is an inbuilt data type
+"""
+
+
+class MyList(list):
+    """
+        This class inherit and appends integers to an object of
+        this class
+    """
+
+    def print_sorted(self):
+        """
+            This method sorts the instance of this class
+        """
+        self = sorted(self)
+        print(self)
+
+
