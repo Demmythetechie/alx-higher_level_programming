8e4004634499578e7f9513b3a5d40bf5ed0daca3
demmythetechie@gmail.com <demmythetechie@gmail.com>
2023-01-11 21:00:42 +0000

    4. From JSON string to Object

---
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+import json
+
+""" This module deals with json representation """
+
+
+def to_json_string(my_obj):
+    """
+        This converts python object of list to json object
+        using dumps which returns the json object as a string
+    """
+
+    return json.dumps(my_obj)
