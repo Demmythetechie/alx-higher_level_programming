a73a541e33863ac8be8c700b5f0ef106503ac5db
demmythetechie@gmail.com <demmythetechie@gmail.com>
2023-01-11 23:56:20 +0000

    8. Class to JSON

---
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+""" This module deal with json representation """
+import json
+
+
+def class_to_json(obj):
+    """
+        This function converts python object
+        to json object and returns it to a string
+    """
+
+    res = {}
+    if hasattr(obj, "__dict__"):
+        res = obj.__dict__.copy()
+    return res
