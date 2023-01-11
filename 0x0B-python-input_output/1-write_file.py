6fdb8750bbcd55b5cbb1d3fe31a3e37d03534d53
demmythetechie@gmail.com <demmythetechie@gmail.com>
2023-01-11 19:52:45 +0000

    1. Write to a file

---
+++ 0x0B-python-input_output/1-write_file.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+""" This module modifies a file """
+
+def write_file(filename="", text=""):
+    """
+        This functions write a text to a file.
+        If the file is not created it automatically
+        creates it else it overwrite if it exist
+    """
+
+    with open(filename, 'w+', encoding="utf-8") as f:
+        return f.write(text)
--- 0x0B-python-input_output/README.md
+++ 0x0B-python-input_output/README.md
@@ -1,1 +1,2 @@
 Task 0: This function reads the content in the textfile and prints it out to standard output
+Task 1: This functions write a text to a file. If the file is not created it automatically creates it else it overwrite if it exist
