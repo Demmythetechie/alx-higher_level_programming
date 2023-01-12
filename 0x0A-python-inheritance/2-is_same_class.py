9c3a80f2143f2e36633599c177372c2b984859ba
demmythetechie@gmail.com <demmythetechie@gmail.com>
2023-01-12 02:26:00 +0000

    2. Exact same object

--- 0x0A-python-inheritance/2-is_same_class.py
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -10,4 +10,7 @@ def is_same_class(obj, a_class):
         using isinstance method
     """
 
-    return isinstance(obj, a_class)
+    if type(obj) == a_class:
+        return True
+    else:
+        return False
