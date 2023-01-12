dff9a674a9d1a874d9525bed99059e71f41f4b00
demmythetechie@gmail.com <demmythetechie@gmail.com>
2023-01-12 03:45:00 +0000

    7. Integer validator7. Integer validator

--- 0x0A-python-inheritance/7-base_geometry.py
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -14,6 +14,6 @@ class BaseGeometry:
         """This function validates whether value is type int and >= 0"""
 
         if type(value) != int:
-            raise TypeError("<name> must be an integer")
+            raise TypeError(f"{name} must be an integer")
         elif value <= 0:
-            raise ValueError("<name> must be greater than 0")
+            raise ValueError(f"{name} must be greater than 0")
