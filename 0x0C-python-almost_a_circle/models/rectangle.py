9d135cb656d77bcbf9986759b4ddc95416f74f68
demmythetechie@gmail.com <demmythetechie@gmail.com>
2023-01-18 12:08:13 +0000

    6. __str__

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -173,5 +173,5 @@ class Rectangle(Base):
         """
 
         return ("[Rectangle] ({}) {}/{} - {}/{}"
-              .format(self.id, self.__x,
-                      self.__y, self.__width, self.__height))
+                .format(self.id, self.__x,
+                        self.__y, self.__width, self.__height))
