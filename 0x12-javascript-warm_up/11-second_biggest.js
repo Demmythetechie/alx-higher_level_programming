e250037da6a2a2e4ec5cda50b866a2931c22544a
demmythetechie@gmail.com <demmythetechie@gmail.com>
2023-03-14 12:27:09 +0000

    11. Second biggest!

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,11 +1,12 @@
 #!/usr/bin/node
-let arg = [];
+
+const arg = [];
 for (let i = 2; i < process.argv.length; i++) {
   arg.push(Number(process.argv[i]));
 }
 if (arg.length <= 1) {
   console.log(0);
 } else {
-  arg.sort(function(a, b){return b - a});
+  arg.sort(function (a, b) { return b - a; });
   console.log(arg[1]);
 }
