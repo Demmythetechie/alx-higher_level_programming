7a80ec3ce11cc25dfa990297c2122f1f19c44179
demmythetechie@gmail.com <demmythetechie@gmail.com>
2023-03-14 10:37:18 +0000

    6. Loop to languages

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 
-const str = ['C is fun', 'python is cool', 'JavaScript is amazing'];
+const str = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
 let i = 0;
 while (i < str.length) {
   console.log(str[i]);
