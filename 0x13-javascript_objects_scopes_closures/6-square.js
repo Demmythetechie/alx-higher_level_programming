ee8704dc6b4aa2343a44f8f00940306c413d725b
Demmythetechie <109427907+Demmythetechie@users.noreply.github.com>
2023-05-12 07:32:48 +0100

    Delete 0x13-javascript_objects_scopes_closures directory

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/node
-const Squaree = require('./5-square.js')
-
-module.exports = class Square extends Squaree {
-  charPrint(c='X') {
-    super.print(c)
-  }
-}
--- 0x13-javascript_objects_scopes_closures/README.md
+++
@@ -1,2 +0,0 @@
-Task 0: Created an empty class Rectangle
-Task 1: Added and initialize instance w and h to the class
