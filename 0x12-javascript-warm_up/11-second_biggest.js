6dc7f72320ed8c42859d3cd3966885941b4023f7
Demmythetechie <demmythetechie@gmail.com>
2023-06-16 14:16:29 +0000

    11. Second biggest!

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -3,7 +3,7 @@ let argv = process.argv;
 if (argv.length <= 3) {
   console.log('0');
 } else {
-  argv = argv.slice(2, argv.length - 2);
+  argv = argv.slice(2, argv.length - 3);
   argv = argv.map(Number);
   argv.sort(function (a, b) {
     return b - a;
