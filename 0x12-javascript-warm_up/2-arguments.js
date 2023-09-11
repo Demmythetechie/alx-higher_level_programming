0c0d32dd51cf24b3eb3d01b30c928dd2c1bc813a
Demmythetechie <demmythetechie@gmail.com>
2023-09-11 22:36:09 +0000

    2. Arguments

---
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -0,0 +1,14 @@
+#!/usr/bin/node
+
+/*
+ * This script that prints a message depending on
+ * the number of arguments passed
+ */
+
+if (process.argv.length === 3) {
+	console.log("Argument found");
+} else if (process.argv.length >= 4){
+	console.log("Arguments found");
+} else {
+	console.log("No argument");
+}
