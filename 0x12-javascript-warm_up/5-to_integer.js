4e0334cd04ba3435755be4f25e0507c7613a0b51
Demmythetechie <demmythetechie@gmail.com>
2023-09-12 04:09:36 +0000

    5. An Integer

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -6,7 +6,7 @@
  */
 
 if (Number(process.argv[2])) {
-  console.log(Math.floor(Number(process.argv[2])));
+  console.log('My number: ' + Math.floor(Number(process.argv[2])));
 } else {
   console.log('Not a number');
 }
