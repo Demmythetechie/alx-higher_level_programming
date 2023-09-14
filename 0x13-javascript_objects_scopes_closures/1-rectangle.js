e06a17e463dc04237c14b861b50917d3a399e937
Demmythetechie <demmythetechie@gmail.com>
2023-09-14 11:00:33 +0000

    1. Rectangle #1

---
+++ 0x13-javascript_objects_scopes_closures/1-rectangle.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+
+// This script creates an empty class Rectangle that defines a rectangle
+
+class Rectangle {
+  constructor (w, h) {
+    this.width = w;
+    this.height = h;
+  }
+}
+
+module.exports = Rectangle;
