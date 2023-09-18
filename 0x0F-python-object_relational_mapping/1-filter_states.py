c66149c7b20897770713d9081e49f123c5ce9b83
Demmythetechie <demmythetechie@gmail.com>
2023-09-18 20:22:02 +0000

    1. Filter states

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -23,7 +23,7 @@ if __name__ == '__main__':
     cur = engine.cursor()
     cur.execute("""
             SELECT * FROM states
-            WHERE name LIKE 'N%' AND name LIKE 'n%'
+            WHERE name LIKE BINARY 'N%'
             ORDER BY id ASC;
     """)
     rows = cur.fetchall()
