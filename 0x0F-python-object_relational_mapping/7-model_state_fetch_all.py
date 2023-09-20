cc23f6fd5f701e85a72b05a01c737114fc1293ca
Demmythetechie <demmythetechie@gmail.com>
2023-09-20 17:48:51 +0000

    7. All states via SQLAlchemy

---
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+
+"""
+This script list all State objects from the database hbtn_0e_6_usa
+"""
+
+from sys import argv
+from model_state import Base, State
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy import create_engine
+
+engine = create_engine(
+        'mysql+mysqldb://{}:{}@localhost/{}'.format(argv[1], argv[2], argv[3])
+        )
+Base.metadata.create_all(engine)
+Session = sessionmaker(bind=engine)
+session = Session()
+rows = session.query(State).all()
+for row in rows:
+    print('{}{}{}'.format(row.id, ': ', row.name))
