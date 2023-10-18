449b1c500cbce77a86540bc86a9cb927918e823f
Demmythetechie <demmythetechie@gmail.com>
2023-10-18 14:32:53 +0100

    0. What's my status? #0

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -15,10 +15,8 @@ if __name__ == "__main__":
     with urlopen("https://alx-intranet.hbtn.io/status") as response:
         content = response.read()
         response.close()
-    print(
-        """
-    Body response:$
-        - type: {}
-        - content: {}
-        - utf8 content: {}
-        """.format(type(content), content, content.decode('utf-8')))
+    print("""Body response:
+    - type: {}
+    - content: {}
+    - utf8 content: {}""". format(
+        type(content), content, content.decode('utf-8')))
