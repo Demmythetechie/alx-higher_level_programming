922326212d5f968fa60fd09620ac62f05c358169
Demmythetechie <demmythetechie@gmail.com>
2023-10-18 20:28:29 +0100

    2. POST an email #0

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -5,6 +5,7 @@ send a post request with a variable
 email and also display the body response
 """
 from urllib.request import Request, urlopen
+import urllib.parse as parse
 from sys import argv
 
 
@@ -14,8 +15,11 @@ if __name__ == "__main__":
     send a post request with a variable
     email and also display the body response
     """
+    value = {'email': argv[2]}
+    data = parse.urlencode(value)
+    data = data.encode('utf-8')
+    post = Request(argv[1], data)
 
-    post = Request(argv[1], headers={'email': argv[2]})
     try:
         with urlopen(post) as response:
             data = response.read()
