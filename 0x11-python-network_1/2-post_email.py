913308f7486f450745d2d6521cfacb397f67774f
Demmythetechie <demmythetechie@gmail.com>
2023-10-18 19:29:24 +0100

    2. POST an email #0

---
+++ 0x11-python-network_1/2-post_email.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""
+This script takes in a url and
+send a post request with a variable
+email and also display the body response
+"""
+from urllib.request import Request, urlopen
+from sys import argv
+
+
+if __name__ == "__main__":
+    """
+    This script takes in a url and
+    send a post request with a variable
+    email and also display the body response
+    """
+
+    post = Request(argv[1], data=None, headers={'email': argv[2]})
+    try:
+        with urlopen(post) as response:
+            data = response.read()
+        print(data)
+    except:
+        pass
