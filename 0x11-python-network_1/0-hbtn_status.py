e2b539a79241c8590ff64dc85bba8b6499f93247
Demmythetechie <demmythetechie@gmail.com>
2023-10-18 13:35:25 +0100

    0. What's my status? #0

---
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""
+This py script fetches data from
+https://alx-intranet.hbtn.io/status
+"""
+from urllib.request import urlopen
+
+
+if __name__ == "__main__":
+    """
+    Usage of urlopen along with "with" to get data
+    from https://alx-intranet.hbtn.io/status
+    """
+
+    with urlopen("https://alx-intranet.hbtn.io/status") as response:
+        content = response.read()
+        response.close()
+    print(
+        """
+    Body response:$
+        - type: {}
+        - content: {}
+        - utf8 content: {}
+        """.format(type(content), content, content.decode('utf-8')))
