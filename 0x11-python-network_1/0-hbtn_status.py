70136cbd21af1d12ba068b2cdf65f85c8d2f33de
Demmythetechie <demmythetechie@gmail.com>
2023-10-18 16:18:15 +0100

    0. What's my status? #0

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -18,6 +18,6 @@ if __name__ == "__main__":
     types = type(content)
     utf = content.decode('utf-8')
     print("""Body response:
-- type: {}
-- content: {}
-- utf8 content: {}""".format(types, content, utf))
+    - type: {}
+    - content: {}
+    - utf8 content: {}""".format(types, content, utf))
