f64ebd89ddad5e6e2ad31fecef848c32e517c5ed
Demmythetechie <demmythetechie@gmail.com>
2023-10-19 04:47:30 +0100

    4. What's my status? #1

---
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""
+This script uses request to fetch data from the url 
+specified
+"""
+import requests
+
+
+if  __name__ == "__name__":
+    """
+    This script uses request to fetch data
+    from the url specified
+    """
+
+    response = requests.get('https://alx-intranet.hbtn.io/status')
+
+    print("""Body response:
+    \t- type: {}
+    \t- content: {}""".format(type(response.text), response.text), end='\n')
