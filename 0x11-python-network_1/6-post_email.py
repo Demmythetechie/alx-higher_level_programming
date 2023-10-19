65825c1a0878fded27ac8c61cd1590967fd7c111
Demmythetechie <demmythetechie@gmail.com>
2023-10-19 09:51:15 +0100

    6. POST an email #1

--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -14,4 +14,4 @@ if __name__ == "__main__":
     """
 
     response = requests.post(argv[1], data={'email': argv[2]})
-    print("Your email is: {}".format(response.text))
+    print("{}".format(response.text))
