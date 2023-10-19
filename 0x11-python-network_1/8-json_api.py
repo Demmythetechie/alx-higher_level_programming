3f957d58e66a8dca352dd67900db079cd80b0c69
Demmythetechie <demmythetechie@gmail.com>
2023-10-19 20:50:10 +0100

    9. My GitHub!

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -22,7 +22,7 @@ if __name__ == "__main__":
     response = requests.post(url, data={'q': value})
     try:
         js = response.json()
-    except:
+    except Exeception as e:
         js = None
 
     if type(js) == dict and len(js) != 0:
