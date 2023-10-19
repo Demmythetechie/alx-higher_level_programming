a00ebfd54bf5898a8e5316307f33daf87882ed1d
Demmythetechie <demmythetechie@gmail.com>
2023-10-19 13:31:46 +0100

    8. Search API

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -4,9 +4,9 @@ This script takes in a url and sends
 a post request with the parameter 'q' with
 or without a value
 """
+
 import requests
 from sys import argv
-
 
 if __name__ == "__main__":
     """
