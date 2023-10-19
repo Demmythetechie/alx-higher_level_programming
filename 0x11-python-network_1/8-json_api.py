1d9f97984eefae89c98369bf3e003f3210083d0a
Demmythetechie <demmythetechie@gmail.com>
2023-10-19 13:29:29 +0100

    8. Search API

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 """
 This script takes in a url and sends
-a post request with the parameter q
+a post request with the parameter 'q' with
+or without a value
 """
 import requests
 from sys import argv
@@ -10,17 +11,18 @@ from sys import argv
 if __name__ == "__main__":
     """
     This script takes in a url and sends
-    a post request with the parameter q
+    a post request with the parameter q with
+    or without a value
     """
 
     url = 'http://0.0.0.0:5000/search_user'
+    response = requests.post(url, data={'q': value})
+    js = response.json()
     if argv[1]:
         value = argv[1]
     else:
         value = ""
 
-    response = requests.post(url, data={'q': value})
-    js = response.json()
     if type(js) == dict and len(js) != 0:
         print('[{}] {}'.format(js.get("id"), js.get("name")))
     else if type(js) != dict:
