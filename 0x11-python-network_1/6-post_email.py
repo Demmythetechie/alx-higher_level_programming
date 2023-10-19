d4f4a6d1a388611e76135cd20d8da9191970e4a9
Demmythetechie <demmythetechie@gmail.com>
2023-10-19 09:44:23 +0100

    6. POST an email #1

---
+++ 0x11-python-network_1/6-post_email.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""
+This script takes in Url and sends a post request
+to the pass Url with the parameter variable
+"""
+import requests
+from sys import argv
+
+
+if __name__ == "__main__":
+    """
+    This script takes in Url and sends a post request
+    to the pass Url with the parameter variable
+    """
+
+    response = requests.post(argv[1], data={'email': argv[2]})
+    print("Your email is: {}".format(response.text))
